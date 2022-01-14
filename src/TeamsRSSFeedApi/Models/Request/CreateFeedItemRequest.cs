2f074ac09830a8cb0301f6f888fdd34e930fee77
Martin Smith <martin.smith@clear.bank>
2022-01-14 10:54:27 +0000

    Boilerplate API with initial Feed Controller

---
+++ src/TeamsRSSFeedApi/Models/Request/CreateFeedItemRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeamsRSSFeedApi.Models.Request
+{
+    public class CreateFeedItemRequest
+    {
+        [Required]
+        public string Text { get; set; }
+
+        public string Category { get; set; }    // TODO: Different to Feed.Title ?
+
+        public string Importance { get; set; }  // TODO: Enum ?
+
+        public string[] Tags { get; set; }
+    }
+}
